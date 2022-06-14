61545e96a50e670ceed7621770ad0b4cd507469f
Mark Howe <mvahowe@gmail.com>
2022-06-14 15:16:15 +0200

    USFM helps

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const perf2usfm = require('./perf2usfm');
 const Validator = require("./validator");
+const usfmHelps = require("./usfmHelps");
 
 module.exports = {
     perf2usfm,
     Validator,
+    usfmHelps,
 };
---
+++ test/code/usfmHelps.cjs
@@ -0,0 +1,21 @@
+import test from 'tape';
+import path from 'path';
+import fse from 'fs-extra';
+import usfmHelps from '../../src/usfmHelps.js';
+
+const testGroup = 'usfmHelps';
+
+test(
+    `Data present (${testGroup})`,
+    async function (t) {
+        try {
+            t.plan(10);
+            for (const v of ["characterTags", "bodyTags", "headingTags", "introHeadingTags", "introBodyTags"]) {
+                t.ok(v in usfmHelps);
+                t.ok(usfmHelps[v].length > 0);
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    },
+);
