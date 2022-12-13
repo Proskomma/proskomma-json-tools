4dd5b7f40e7d6fb6164d806aaad20a4a49b1a0bb
Mark Howe <mvahowe@gmail.com>
2022-12-13 17:58:48 +0100

    Test for alleged issue with premature ts

--- test/code/test_usfm2perf_pipeline.cjs
+++ test/code/test_usfm2perf_pipeline.cjs
@@ -12,6 +12,8 @@ const validator = new Validator();
 
 const usfmContent = fse.readFileSync(path.resolve(__dirname, '../test_data/usfms/titus.usfm')).toString();
 
+const usfmContent2 = fse.readFileSync(path.resolve(__dirname, '../test_data/usfms/ult_tit_misplaced_ts.usfm')).toString();
+
 test(`usfm=>perf : validate the output perf (${testGroup})`, async (t) => {
     t.plan(1);
     try {
@@ -19,6 +21,31 @@ test(`usfm=>perf : validate the output perf (${testGroup})`, async (t) => {
             usfm: usfmContent,
             selectors: {'lang': 'fra', 'abbr': 'ust'}
         });
+
+        const validatorResult = validator.validate('constraint','perfDocument','0.2.1', output.perf);
+        if (!validatorResult.isValid) {
+            t.fail('usfm=>perf throws on valid usfm');
+            throw `usfm=>perf, PERF file is not valid. \n${JSON.stringify(validatorResult,null,2)}`;
+        } else {
+            t.ok(validatorResult.isValid);
+        }
+
+        // await saveFile(JSON.stringify(output.perf, null, 2));
+    } catch (err) {
+        console.log(err);
+        t.fail('usfm2perfPipeline throws on valid perf');
+    }
+});
+
+test(`usfm has premature ts milestone (${testGroup})`, async (t) => {
+    t.plan(1);
+    try {
+        let output = await pipelineH.runPipeline('usfm2perfPipeline', {
+            usfm: usfmContent2,
+            selectors: {'lang': 'fra', 'abbr': 'ult'}
+        });
+
+        console.log(JSON.stringify(output.perf.sequences, null, 2));
 
         const validatorResult = validator.validate('constraint','perfDocument','0.2.1', output.perf);
         if (!validatorResult.isValid) {
