4efd6bf22da6682d350ab75f0f25b59a4dccfb6a
Mark Howe <mvahowe@gmail.com>
2023-12-14 16:37:16 +0100

    Produce most headers re #32

--- src/render/perfToX/renderActions/perfToUsfmJs.js
+++ src/render/perfToX/renderActions/perfToUsfmJs.js
@@ -1,10 +1,24 @@
+// Put mt into headers
+
 const perfToUsfmJsActions = {
     startDocument: [
         {
             description: "Setup",
             test: () => true,
-            action: (environment) => {
-                environment.output.usfmJs = {};
+            action: ({context, workspace, output}) => {
+                workspace.chapter = null;
+                workspace.verses = null;
+                output.usfmJs = {headers: []};
+                for (const [k,v] of Object.entries(context.document.metadata.document)) {
+                    if (['bookCode', 'properties', 'tags'].includes(k)) {
+                        continue;
+                    }
+                    output.usfmJs.headers.push({
+                        tag: k === "toc" ? "toc1" : k,
+                        content: v
+                    });
+
+                }
             }
         }
     ]
--- test/code/test_perfToUsfmJsPipeline.cjs
+++ test/code/test_perfToUsfmJsPipeline.cjs
@@ -1,17 +1,24 @@
-const {PipelineHandler} = require('../../dist/index');
+const path = require('path');
+const fse = require('fs-extra');
 const test = require('tape');
 const {Proskomma} = require('proskomma');
-const fse = require('fs-extra');
-const path = require('path');
-
+const {PipelineHandler} = require('../../dist/index');
+const usfmJsPackage = require('usfm-js');
 const testGroup = 'Perf 2 usfmJs';
 
-const pipelineH = new PipelineHandler({proskomma: new Proskomma(), verbose: false});
+const pipelineH = new PipelineHandler({
+    proskomma: new Proskomma(),
+    verbose: false
+});
 
-const perfContent = fse.readJsonSync(path.resolve(__dirname, '../test_data/perfs/titus_aligned_eng.json'));
-
+const usfm = fse.readFileSync(path.resolve(__dirname, '../test_data/usfms/titus_aligned.usfm')).toString();
+const pk = new Proskomma();
+pk.importDocument({lang: "fra", abbr: "lsg"}, "usfm", usfm);
+const perfContent = JSON.parse(pk.gqlQuerySync("{documents {perf}}").data.documents[0].perf);
+const usfmJsJson = usfmJsPackage.toJSON(usfm);
+// console.log("UsfmJs\n", JSON.stringify(usfmJsJson.headers, null, 2));
 test(`perf=>usfmJs (${testGroup})`, t => {
-    t.plan(2);
+    t.plan(5);
     let output;
     try {
         t.doesNotThrow(async () => {
@@ -22,6 +29,11 @@ test(`perf=>usfmJs (${testGroup})`, t => {
             );
         });
         t.ok(output.usfmJs);
+        t.ok(output.usfmJs.headers);
+        const idHeader = output.usfmJs.headers.filter(h => h.tag === "id")[0];
+        t.ok(idHeader);
+        t.ok(idHeader.content.startsWith('TIT'));
+//        console.log("Proskomma\n", JSON.stringify(output.usfmJs.headers, null, 2));
     } catch (err) {
         console.log(err);
         t.fail('perfToUsfmJsPipeline throws on valid perf');
